e3cd5afe548e8f6c86eb0e03a38056b31e39f368
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-31 16:51:11 +0530

    added header folder

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react"
+import "./Header.css"
+import Head from "./Head"
+import Search from "./Search"
+import Navbar from "./Navbar"
 
-const Header = () => {
+const Header = ({ CartItem }) => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Head />
+      <Search CartItem={CartItem} />
+      <Navbar />
+    </>
   )
 }
 
