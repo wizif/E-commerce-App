a872bdf645ebc6269ac3a71450c7063c7de0c268
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-08-02 21:38:24 +0530

    hn

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -14,8 +14,10 @@ const Head = () => {
           <div className='right row RText'>
             <label>Theme FAQ"s</label>
             <label>Need Help?</label>
+            
             <span>🏳️‍⚧️</span>
             <label>EN</label>
+            
             <span>🏳️‍⚧️</span>
             <label>USD</label>
           </div>
